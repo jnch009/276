aea33d7b8d2e79248a86c81555446dbdc44c3f9f
matthewtseng <tsengmatthew@hotmail.com>
2015-11-04 01:11:23 +0000

    fixed database.yml and Gemfile lol

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151020012100) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "usernames", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
