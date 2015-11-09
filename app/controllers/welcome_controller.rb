7da0b9da9158c89f17928229ba1c3a1e274048d8
matthewtseng <tsengmatthew@hotmail.com>
2015-11-09 09:54:43 +0000

    changed titles of homepage and footer

--- app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  before_action :require_user
-  def index
-  end
-
-end
--- test/models/username_test.rb
+++
@@ -1,6 +0,0 @@
-require 'test_helper'
-
-class UsernameTest < ActiveSupport::TestCase
-  test "no first name" do
-  end
-end
