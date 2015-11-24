ec678c4451edf50222f930a28cb1bf0ea5150784
jnch009 <jngchenghin@gmail.com>
2015-11-24 00:53:25 +0000

    Refactored code

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,10 @@
 class SearchesController < ApplicationController
+    def index
+        @searches = Search.all
+    end
+    def show 
+	   @search = Search.find(params[:id])
+    end
     def new 
         @search = Search.new
     end
@@ -6,15 +12,11 @@ class SearchesController < ApplicationController
     def create
         @search = Search.new(search_params)
 		if @search.save
-    		redirect_to @search
+    		render action: "index"
   		else
     		flash.now[:danger] = 'Invalid search combination'   		    
     		render 'new'
   		end
-    end
-  		
-	def show 
-	   @search = Search.find(params[:id])
     end
     
     private
