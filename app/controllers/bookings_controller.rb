d3b624f465eb3652451d137da2dd90d5f13ab6ff
jnch009 <jngchenghin@gmail.com>
2015-11-25 21:10:18 +0000

    Flash message showing after booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,6 +7,7 @@ class BookingsController < ApplicationController
     def create
         @booking = Booking.new(booking_params)
 		if @booking.save
+		    flash[:notice] = 'Successfully booked this restaurant!'
     		redirect_to root_path
     		#render @search
   		else
