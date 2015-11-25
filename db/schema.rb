223b8ef409ff4f110c7a99767aa331b40b7fb555
zzh001 <zhihaoz@sfu.ca>
2015-11-25 01:47:28 +0000

    Profile page updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124032346) do
+ActiveRecord::Schema.define(version: 20151124061506) do
+
+  create_table "bookings", force: :cascade do |t|
+    t.string   "FirstName"
+    t.string   "LastName"
+    t.string   "Phone"
+    t.string   "Time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "searches", force: :cascade do |t|
     t.string   "restname"
     t.integer  "slimit"
     t.string   "location"
+    t.string   "identifier"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "identifier"
   end
 
   create_table "users", force: :cascade do |t|
