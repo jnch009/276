14579a43f049fb95af4520816c0fbcef95b27fe8
jnch009 <jngchenghin@gmail.com>
2015-11-28 13:21:04 +0000

    bookings working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,18 @@
 class BookingsController < ApplicationController
     before_action :logged_in_user
+    def index
+        @bookings = Booking.all
+    end
     def new
         @booking = Booking.new
         @identity = params[:identity]
-        Rails.cache.write("name",params[:identity])
+        Rails.cache.write("name",@identity)
     end
     def create
-        @booking = Booking.new(booking_params)
+        @booking = current_user.bookings.build(booking_params)
 		if @booking.save
 		    flash[:notice] = 'Successfully booked this restaurant'
-    		redirect_to new_history_path(:name => Rails.cache.read("name"))
+		    redirect_to new_history_path(:name => Rails.cache.read("name"))
   		else
     		flash.now[:danger] = 'Invalid search combination'   		    
     		render 'new'
