02ab3042a13135dbc36bbce5a5a1ad976f19ba5d
jnch009 <jngchenghin@gmail.com>
2015-11-28 08:56:46 +0000

    validated offset

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,5 @@
 class Search < ActiveRecord::Base
     validates :location, presence: true
     validates :slimit, numericality: {only_integer: true,greater_than_or_equal_to: 1,less_than_or_equal_to:20}
+    validates :offset, presence: true
 end
