07a75c5969a3f46dbddf17540dde0ce365a8579f
jnch009 <jngchenghin@gmail.com>
2015-11-28 04:10:07 +0000

    pushing to heroku

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :logged_in_user
+  before_action :logged_in_user, only: [:new,:create]
   def index
     @reviews = Review.all
   end
