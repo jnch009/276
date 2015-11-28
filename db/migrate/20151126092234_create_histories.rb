264eb4db48cd77f05ea66d2439e8f40b41d02f2a
jnch009 <jngchenghin@gmail.com>
2015-11-28 09:43:45 +0000

    created index and reference for Bookings

--- db/migrate/20151126092234_create_histories.rb
+++ db/migrate/20151126092234_create_histories.rb
@@ -5,8 +5,9 @@ class CreateHistories < ActiveRecord::Migration
       t.string :LastName
       t.string :Phone
       t.string :Time
-
+      t.references :user, index: true, foreign_key: true
       t.timestamps null: false
     end
+    add_index :histories, [:user_id, :created_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126092234) do
+ActiveRecord::Schema.define(version: 20151128093108) do
 
   create_table "bookings", force: :cascade do |t|
     t.string   "FirstName"
@@ -22,7 +22,11 @@ ActiveRecord::Schema.define(version: 20151126092234) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "bookings", ["user_id", "created_at"], name: "index_bookings_on_user_id_and_created_at"
+  add_index "bookings", ["user_id"], name: "index_bookings_on_user_id"
 
   create_table "histories", force: :cascade do |t|
     t.string   "FirstName"
