764c6dd3b88f4141ec77de3b31d469325581c582
ejli <li.edwin@hotmail.com>
2015-12-01 01:09:41 +0000

    stylized pages

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,16 @@
 ActiveRecord::Schema.define(version: 20151130082004) do
 
   create_table "bookings", force: :cascade do |t|
-    t.string   "time"
+    t.string   "FirstName"
+    t.string   "LastName"
+    t.string   "Phone"
+    t.string   "Time"
     t.string   "restaurant"
     t.string   "address"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.date     "restaurant_date"
+    t.date     "rdate"
   end
 
   add_index "bookings", ["user_id", "created_at"], name: "index_bookings_on_user_id_and_created_at"
@@ -42,7 +45,6 @@ ActiveRecord::Schema.define(version: 20151130082004) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.string   "restaurants"
-    t.string   "stars"
   end
 
   add_index "reviews", ["user_id", "created_at"], name: "index_reviews_on_user_id_and_created_at"
